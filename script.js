8491758115316f0184a8f320f2aa713b76f71a4f
ZnAkI <sodayt36@gmail.com>
2025-03-15 17:43:46 +0700

    Добавил портфолио

---
+++ script.js
@@ -0,0 +1,19 @@
+﻿// Меняем цвет заголовка при клике
+
+
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const sectionId = link.getAttribute('href');
+        document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
